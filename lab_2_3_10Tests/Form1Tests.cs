cd0b20c615fc86f8288961ba02e660496ff403f2
LudmilaYe <yegupova.mila@bk.ru>
2021-10-01 17:11:14 +0800

    Добавлены автотесты

--- lab_2_3_10Tests/Form1Tests.cs
+++ lab_2_3_10Tests/Form1Tests.cs
@@ -9,5 +9,17 @@ using System.Threading.Tasks;
 namespace lab_2_3_10.Tests {
   [TestClass()]
   public class Form1Tests {
+    [TestMethod()]
+    public void GetWordsFromSentenceTest1() {
+      CollectionAssert.AreEqual(new string[] { "наша", "таня", "громко", "плачет" }, Form1.GetWordsFromSentence("Наша Таня громко плачет:"));
+    }
+    [TestMethod()]
+    public void GetWordsFromSentenceTest2() {
+      CollectionAssert.AreEqual(new string[] { "уронила", "в", "речку", "мячик" }, Form1.GetWordsFromSentence("Уронила:;; в речку мячик.    ,,,:"));
+    }
+    [TestMethod()]
+    public void GetWordsFromSentenceTest3() {
+      CollectionAssert.AreEqual(new string[] { "речка", "гречка" }, Form1.GetWordsFromSentence("РЕЧКА ГРЕчкА реЧКА гречка"));
+    }
   }
 }
